d72541fac4e61cacdcfbb9eac5a03d8aeeebc160
Kristof Kalai <kristof98@gmail.com>
2022-03-17 22:50:43 +0100

    chore: try to add Resolver

--- Package.swift
+++ Package.swift
@@ -12,7 +12,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", .exact("4.4.0")),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", .exact("2.2.4")),
         .package(url: "https://github.com/nodes-vapor/gatekeeper.git", .exact("4.2.0")),
-        .package(url: "https://github.com/vapor-community/sendgrid.git", .exact("4.0.0"))
+        .package(url: "https://github.com/vapor-community/sendgrid.git", .exact("4.0.0")),
+        .package(url: "https://github.com/hmlongco/Resolver.git", from: "1.5.0")
     ],
     targets: [
         .target(
@@ -22,7 +23,8 @@ let package = Package(
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Gatekeeper", package: "gatekeeper"),
-                .product(name: "SendGrid", package: "sendgrid")
+                .product(name: "SendGrid", package: "sendgrid"),
+                .product(name: "Resolver", package: "Resolver")
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/DI/Components/Initable.swift
+++ Sources/App/DI/Components/Initable.swift
@@ -5,6 +5,8 @@
 //  Created by Kristof Kalai on 2022. 03. 14..
 //
 
+import Resolver
+
 protocol Initable {
     init()
 }
