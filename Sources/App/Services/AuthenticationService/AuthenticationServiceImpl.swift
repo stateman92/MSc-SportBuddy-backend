2e339fae8abe958122988f2e15412942496e5475
Kristof Kalai <kristof98@gmail.com>
2022-09-24 18:05:56 +0200

    feat: change to swift 5.7 syntax

--- Sources/App/Services/AuthenticationService/AuthenticationServiceImpl.swift
+++ Sources/App/Services/AuthenticationService/AuthenticationServiceImpl.swift
@@ -16,7 +16,7 @@ extension AuthenticationServiceImpl: AuthenticationService {
     /// - Parameter password: the plain text password.
     /// - Returns: The hashed password.
     func hash(password: String?) -> String? {
-        guard let password = password, isValid(password: password) else { return nil }
+        guard let password, isValid(password: password) else { return nil }
         return try? Bcrypt.hash(password)
     }
 
@@ -25,7 +25,7 @@ extension AuthenticationServiceImpl: AuthenticationService {
     /// - Parameter hashedPassword: the hashed password.
     /// - Returns: Whether the password is correct.
     func isValid(password: String?, hashedPassword: String) -> Bool {
-        guard let password = password else { return false }
+        guard let password else { return false }
         return (try? Bcrypt.verify(password, created: hashedPassword)) == true
     }
 
